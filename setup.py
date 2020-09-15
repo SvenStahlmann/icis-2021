ef89aceee5bd8a0ac60d8aa77283d932408dfc90
Sven Stahlmann <stahlmann.sven.de@gmail.com>
2020-09-15 14:03:06 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for the 2021 ECIS Paper Deep Learning Enabled Consumer Research',
+    author='Sven Stahlmann',
+    license='MIT',
+)
