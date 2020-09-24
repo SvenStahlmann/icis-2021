3026dbd1ed20ab9beab617fcffbc65aef0176e52
Sven S <stahlmann.sven.de@gmail.com>
2020-09-24 14:45:30 +0200

    implemented 10 fold cross validation for SVM

--- src/models/train_svm_model.py
+++ src/models/train_svm_model.py
@@ -1,26 +1,26 @@
 import pandas as pd
 import click
-import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn import model_selection, svm
-
-def load_data(trainingdata, testdata, validdata):
-    train = pd.read_csv(trainingdata, delimiter=',')
-    test = pd.read_csv(testdata, delimiter=',')
-    valid = pd.read_csv(validdata, delimiter=',')
-    return train, test, valid
+from sklearn import svm
 
 
-@click.command()
-@click.option('--trainingdata', default='../../data/processed/in-cat-train.csv', help='path to the training data.')
-@click.option('--testdata', default='../../data/processed/in-cat-test.csv', help='path to the test data.')
-@click.option('--validata', default='../../data/processed/out-of-cat-valid.csv', help='path to the validation data.')
-def main(trainingdata, testdata, validata):
-    train, test, valid = load_data(trainingdata, testdata, validata)
-    print("data loaded")
-    print(f"train: {len(train)}, test: {len(test)}, valid: {len(valid)}")
+def load_validation_data(valid_in_cat_path, valid_out_of_cat_path):
+    valid_in_cat = pd.read_csv(valid_in_cat_path, delimiter=',')
+    valid_out_of_cat = pd.read_csv(valid_out_of_cat_path, delimiter=',')
+
+    return valid_in_cat, valid_out_of_cat
+
+
+def load_fold_data(path, current_fold):
+    fold_train = pd.read_csv(path + 'fold-' + str(current_fold) + '-train.csv', delimiter=',')
+    fold_test = pd.read_csv(path + 'fold-' + str(current_fold) + '-test.csv', delimiter=',')
+
+    return fold_train, fold_test
+
+
+def prediction_svm(train, test, valid, report_df, current_fold, use_tfidf=False):
+    name = 'fold-' + str(current_fold)
     x_train = train['text']
     y_train = train['labels']
     x_test = test['text']
@@ -28,52 +28,69 @@ def main(trainingdata, testdata, validata):
     x_valid = valid['text']
     y_valid = valid['labels']
 
-    #bag of words
-    vectorizer = CountVectorizer(max_features=2000)
+    if use_tfidf:
+        vectorizer = TfidfVectorizer(max_features=2000)
+    else:
+        vectorizer = CountVectorizer(max_features=2000)
+
     vectorizer.fit(x_train)
 
-    x_train_bow = vectorizer.transform(x_train)
-    x_test_bow = vectorizer.transform(x_test)
-    x_valid_bow = vectorizer.transform(x_valid)
+    x_train = vectorizer.transform(x_train)
+    x_test = vectorizer.transform(x_test)
+    x_valid = vectorizer.transform(x_valid)
 
-    #tf idf
-    tfidf_vect = TfidfVectorizer(max_features=2000)
-    tfidf_vect.fit(x_train)
+    model = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+    model.fit(x_train, y_train)
 
-    x_train_tfidf = tfidf_vect.transform(x_train)
-    x_test_tfidf = tfidf_vect.transform(x_test)
-    x_valid_tfidf = tfidf_vect.transform(x_valid)
+    prediction_svm_test = model.predict(x_test)
+    prediction_svm_valid = model.predict(x_valid)
 
-    SVM_bow = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-    SVM_tfidf = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-    SVM_bow.fit(x_train_bow, y_train)
-    SVM_tfidf.fit(x_train_tfidf, y_train)
+    acc_test = accuracy_score(prediction_svm_test, y_test) * 100
+    acc_valid = accuracy_score(prediction_svm_valid, y_valid) * 100
+    f1_test = f1_score(prediction_svm_test, y_test) * 100
+    f1_valid = f1_score(prediction_svm_valid, y_valid) * 100
+    report_df.append([name, acc_test, acc_valid, f1_test, f1_valid])
 
-    prediction_svm_test_bow = SVM_bow.predict(x_test_bow)
-    prediction_svm_valid_bow = SVM_bow.predict(x_valid_bow)
 
-    prediction_svm_test_tfidf = SVM_tfidf.predict(x_test_tfidf)
-    prediction_svm_valid_tfidf = SVM_tfidf.predict(x_valid_tfidf)
+@click.command()
+@click.option('--path', default='../../data/processed/', help='path to the training data.')
+@click.option('--valid_in_cat_path', default='../../data/processed/in-cat-test.csv', help='path to the training data.')
+@click.option('--valid_out_of_cat_path', default='../../data/processed/out-of-cat-valid.csv',
+              help='path to the training data.')
+def main(path, valid_in_cat_path, valid_out_of_cat_path):
+    report_df_bow = []
+    report_df_tfidf = []
+    valid_in_cat, valid_out_of_cat = load_validation_data(valid_in_cat_path, valid_out_of_cat_path)
+    print("validation data loaded")
+    print(f"in cat: {len(valid_in_cat)}, out of cat: {len(valid_out_of_cat)}")
+    print("starting fold training")
+    for i in range(10):
+        name = 'fold-' + str(i)
+        print("working on " + name)
+        train, test = load_fold_data(path, i)
+        # BOW
+        prediction_svm(train, valid_in_cat, valid_out_of_cat, report_df_bow, i)
+        # TFIDF
+        prediction_svm(train, valid_in_cat, valid_out_of_cat, report_df_tfidf, i, True)
+        print("done training model fold " + str(i))
 
-    acc_test_bow = accuracy_score(prediction_svm_test_bow, y_test)*100
-    acc_valid_bow = accuracy_score(prediction_svm_valid_bow, y_valid)*100
-    f1_test_bow = f1_score(prediction_svm_test_bow, y_test) * 100
-    f1_valid_bow = f1_score(prediction_svm_valid_bow, y_valid) * 100
+    report_df_bow = pd.DataFrame(report_df_bow,
+                                 columns=['name', 'acc_in_cat', 'f1_in_cat', 'acc_out_of_cat', 'f1_out_of_cat'])
+    report_df_tfidf = pd.DataFrame(report_df_tfidf,
+                                   columns=['name', 'acc_in_cat', 'f1_in_cat', 'acc_out_of_cat', 'f1_out_of_cat'])
 
-    acc_test_tfidf = accuracy_score(prediction_svm_test_tfidf, y_test) * 100
-    acc_valid_tfidf = accuracy_score(prediction_svm_valid_tfidf, y_valid) * 100
-    f1_test_tfidf = f1_score(prediction_svm_test_tfidf, y_test) * 100
-    f1_valid_tfidf = f1_score(prediction_svm_valid_tfidf, y_valid) * 100
+    print("BOW evaluation in cat")
+    print(f"acc: {report_df_bow['acc_in_cat'].mean()}, f1: {report_df_bow['f1_in_cat'].mean()}")
+    print("BOW evaluation out of cat")
+    print(f"acc: {report_df_bow['acc_out_of_cat'].mean()}, f1: {report_df_bow['f1_out_of_cat'].mean()}")
 
-    print("test set evaluation BOW")
-    print(f"acc: {acc_test_bow}, f1: {f1_test_bow}")
-    print("valid set evaluation BOW")
-    print(f"acc: {acc_valid_bow}, f1: {f1_valid_bow}")
+    print("TFIDF evaluation in cat")
+    print(f"acc: {report_df_tfidf['acc_in_cat'].mean()}, f1: {report_df_tfidf['f1_in_cat'].mean()}")
+    print("TFIDF evaluation out of cat")
+    print(f"acc: {report_df_tfidf['acc_out_of_cat'].mean()}, f1: {report_df_tfidf['f1_out_of_cat'].mean()}")
 
-    print("test set evaluation TFIDF")
-    print(f"acc: {acc_test_tfidf}, f1: {f1_test_tfidf}")
-    print("valid set evaluation TFIDF")
-    print(f"acc: {acc_valid_tfidf}, f1: {f1_valid_tfidf}")
+    report_df_bow.to_csv('../../reports/SVM-bow.csv', index=False)
+    report_df_tfidf.to_csv('../../reports/SVM-tfidf.csv', index=False)
 
 
 if __name__ == '__main__':
