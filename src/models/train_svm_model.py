ec5f6ab63f0898761d43de468162ed06bc602cb0
Sven S <stahlmann.sven.de@gmail.com>
2020-10-15 17:53:12 +0200

    added category evaluation to the report

--- src/models/train_svm_model.py
+++ src/models/train_svm_model.py
@@ -19,7 +19,7 @@ def load_fold_data(path, current_fold):
     return fold_train, fold_test
 
 
-def prediction_svm(train, test, valid, report_df, current_fold, use_tfidf=False):
+def prediction_svm(train, test, valid, report_df, current_fold, categories, use_tfidf=False):
     name = 'fold-' + str(current_fold)
     x_train = train['text']
     y_train = train['labels']
@@ -49,7 +49,25 @@ def prediction_svm(train, test, valid, report_df, current_fold, use_tfidf=False)
     acc_valid = accuracy_score(prediction_svm_valid, y_valid) * 100
     f1_test = f1_score(prediction_svm_test, y_test) * 100
     f1_valid = f1_score(prediction_svm_valid, y_valid) * 100
-    report_df.append([name, acc_test, f1_test, acc_valid, f1_valid])
+
+    # per category prediction
+    cat_dict = {}
+    for category in categories:
+        mask = valid['category'] == category
+        category_df = valid[mask]
+        cat_x_valid = category_df['text']
+        cat_x_valid = vectorizer.transform(cat_x_valid)
+        cat_y_valid = category_df['labels']
+        cat_prediction = model.predict(cat_x_valid)
+        cat_acc_valid = accuracy_score(cat_prediction, cat_y_valid) * 100
+        cat_f1_valid = f1_score(cat_prediction, cat_y_valid) * 100
+        cat_dict[category + "-f1"] = cat_f1_valid
+        cat_dict[category + "-acc"] = cat_acc_valid
+
+    report_df.append([name, acc_test, f1_test, acc_valid, f1_valid] + list(cat_dict.values()))
+
+    # return category columns
+    return list(cat_dict.keys())
 
 
 @click.command()
@@ -61,6 +79,8 @@ def main(path, valid_in_cat_path, valid_out_of_cat_path):
     report_df_bow = []
     report_df_tfidf = []
     valid_in_cat, valid_out_of_cat = load_validation_data(valid_in_cat_path, valid_out_of_cat_path)
+    categories = valid_out_of_cat.category.unique()
+
     print("validation data loaded")
     print(f"in cat: {len(valid_in_cat)}, out of cat: {len(valid_out_of_cat)}")
     print("starting fold training")
@@ -69,15 +89,17 @@ def main(path, valid_in_cat_path, valid_out_of_cat_path):
         print("working on " + name)
         train, test = load_fold_data(path, i)
         # BOW
-        prediction_svm(train, valid_in_cat, valid_out_of_cat, report_df_bow, i)
+        prediction_svm(train, valid_in_cat, valid_out_of_cat, report_df_bow, i, categories)
         # TFIDF
-        prediction_svm(train, valid_in_cat, valid_out_of_cat, report_df_tfidf, i, True)
-        print("done training model fold " + str(i))
+        category_columns = prediction_svm(train, valid_in_cat, valid_out_of_cat, report_df_tfidf, i, categories, True)
+        print("done with " + name)
 
     report_df_bow = pd.DataFrame(report_df_bow,
-                                 columns=['name', 'acc_in_cat', 'f1_in_cat', 'acc_out_of_cat', 'f1_out_of_cat'])
+                                 columns=['name', 'acc_in_cat', 'f1_in_cat', 'acc_out_of_cat',
+                                          'f1_out_of_cat'] + category_columns)
     report_df_tfidf = pd.DataFrame(report_df_tfidf,
-                                   columns=['name', 'acc_in_cat', 'f1_in_cat', 'acc_out_of_cat', 'f1_out_of_cat'])
+                                   columns=['name', 'acc_in_cat', 'f1_in_cat', 'acc_out_of_cat',
+                                            'f1_out_of_cat'] + category_columns)
 
     print("BOW evaluation in cat")
     print(f"acc: {report_df_bow['acc_in_cat'].mean()}, f1: {report_df_bow['f1_in_cat'].mean()}")
