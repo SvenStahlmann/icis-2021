721d9b4c1dddfeec99fa5886719421d324b7fc1a
Sven <stahlmann.sven.de@gmail.com>
2021-03-09 09:25:51 +0100

    added train and predict methods

--- src/models/train_roberta_model.py
+++ src/models/train_roberta_model.py
@@ -3,6 +3,26 @@ import pandas as pd
 from simpletransformers.classification import ClassificationModel
 from sklearn.metrics import f1_score, accuracy_score
 
+DEFAULT_ARGS = {'output_dir': '../../models/roberta-base-bs8-e6',
+                 'use_cached_eval_features': False,
+                 'reprocess_input_data': True,
+                 'train_batch_size': 8,
+                 'num_train_epochs': 6,
+                 'fp16': False,
+                 'overwrite_output_dir': True}
+
+
+def train(train_df: pd.DataFrame, args = DEFAULT_ARGS):
+    model = ClassificationModel('roberta', 'roberta-base', num_labels=2, args=args)
+    model.train_model(train_df)
+
+    return model
+
+def predict(df: pd.DataFrame, model, metric):
+    result, model_outputs, wrong_predictions = model.eval_model(df)
+    # todo check what result is
+    return result
+
 
 def load_validation_data(valid_in_cat_path, valid_out_of_cat_path):
     valid_in_cat = pd.read_csv(valid_in_cat_path, delimiter=',')
--- src/models/train_svm_model.py
+++ src/models/train_svm_model.py
@@ -5,6 +5,41 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics import f1_score, accuracy_score
 
 
+def train(train_df: pd.DataFrame, mode: str ='tf-idf'):
+    '''
+    Trains a bag of words or tf-idf based SVM on the provided training dataframe
+    :param train_df: pandas dataframe with column 'text' and 'label'
+    :param mode: when set to 'tf-idf' uses a tf-idf vectorizer, else a bag of words vectorizer
+    :return: returns the trained model an the used vectorizer
+    '''
+    if mode=='tf-idf':
+        vectorizer = TfidfVectorizer(max_features=2000)
+    else:
+        vectorizer = CountVectorizer(max_features=2000)
+
+    x = train_df['text']
+    y = train_df['labels']
+    vectorizer.fit(x)
+    # turn text into bag of words / tf-idf vector
+    x = vectorizer.transform(x)
+
+    model = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+    model.fit(x, y)
+
+    return model, vectorizer
+
+def predict(df: pd.DataFrame,model: svm.SVC, vectorizer, metric):
+    x = df['text']
+    y = df['labels']
+    # turn text into bag of words / tf-idf vector
+    x = vectorizer.transform(x)
+    prediction = model.predict(x)
+
+    return prediction
+
+
+
+
 def load_validation_data(valid_in_cat_path, valid_out_of_cat_path):
     valid_in_cat = pd.read_csv(valid_in_cat_path, delimiter=',')
     valid_out_of_cat = pd.read_csv(valid_out_of_cat_path, delimiter=',')
